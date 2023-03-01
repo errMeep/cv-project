ed3f69dd255d01bac1f76f19bb3e00254df4386b
errMeep <andypark6004@gmail.com>
2023-02-28 20:43:47 -0500

    Removed files and edited contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,10 +8,27 @@ class Contact extends Component {
       phone: '',
     };
   }
+  onChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
-        <div>asd</div>
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="name" onChange={this.onChange} />
+          <label htmlFor="email">Name: </label>
+          <input type="text" name="email" onChange={this.onChange} />
+          <label htmlFor="name">Name: </label>
+          <input type="phone" name="phone" onChange={this.onChange} />
+        </form>
+        <p>name: {this.state.name}</p>
+        <p>email: {this.state.email}</p>
+        <p>phone: {this.state.phone}</p>
       </div>
     );
   }
